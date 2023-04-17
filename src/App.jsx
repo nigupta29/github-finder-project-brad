ce877303059849043c354f526bae51ce12d5c82a
Nikhil Gupta <jerrycodes29@gmail.com>
2023-04-17 11:43:10 +0530

    add basic page layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { BrowserRouter as Router } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Footer from './components/layouts/Footer'
 import Navbar from './components/layouts/Navbar'
+import About from './pages/About'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
 
 export default function App() {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main>Content</main>
-        <footer>Footer</footer>
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </Router>
   )
